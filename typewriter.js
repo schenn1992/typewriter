de86d8dd0fff6b466cc7537dce9da5c8ef5b608d
Steven <schenn1992@gmail.com>
2020-11-03 18:44:23 +0000

    refactor typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
 let timeCount = 0;
+
 for (const char of sentence) {
   setTimeout(() => {
-    process.stdout.write(char + '\n');
+    process.stdout.write(char);
   }, timeCount);
   timeCount += 50;
 };
 
-
+setTimeout(() => {process.stdout.write("\n")}, timeCount);
 
 
