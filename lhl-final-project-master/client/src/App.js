bf05d796cde173923c796d9da68c9bd126884bae
Steven Chen <schenn1992@gmail.com>
2021-02-12 12:32:42 -0500

    remove unnecessary parts of project to begin with

---
+++ lhl-final-project-master/client/src/App.js
@@ -0,0 +1,11 @@
+import Home from './components/Home';
+import Earth from './components/Earth';
+import Mars from './components/Mars';
+
+import './App.css';
+
+function App() {
+  return <div className='App'></div>;
+}
+
+export default App;
